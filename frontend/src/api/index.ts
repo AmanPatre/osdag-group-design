d2288e446729fc9032dd46f42efd42cd7fe7f593
AmanPatre <amanpatre25@gmail.com>
2026-04-09 13:06:12 +0530

    Connected to live API Url

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://osdag-group-design.onrender.com/api';
 
 export interface LocationApiResponse {
   states: Record<string, string[]>;
